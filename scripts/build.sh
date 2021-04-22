1de7208d8dfffba50eb758ec17ac701dda340386
João Neto <jneto@MacJN2.lan>
2021-04-22 09:34:21 +0100

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,21 @@
 #
 # Build cudadev image
 #
-# RUN: ~/dev/lib/cudadev/scripts/deploy.sh
+# RUN: ~/dev/lib/cudadev/scripts/build.sh
 #
 
 # Build image on cuda1
 cd ~/dev/lib/cudadev &&\
 git add . && git commit -m 'update' && git push &&\
 ssh -i ~/.ssh/jn2020 -p 9022 jneto@ml.dlogic.io \
-    "cd ~/lib && rm -rf ml && git clone https://github.com/jn2050/ml.git && cd ml"
+    "cd ~/lib && rm -rf cudadev && git clone https://github.com/jn2050/cudadev.git" &&\
+ssh -i ~/.ssh/jn2020 -p 9022 jneto@ml.dlogic.io \
+    "cd ~/lib/cudadev &&\
+    sudo docker build -t cudadev . &&\"
+    sudo docker tag cudadev digitallogic/private:cudadev &&\
+    sudo docker push digitallogic/private:cudadev"
+
+exit 0
 
 # export img=ops
 export img=cuda

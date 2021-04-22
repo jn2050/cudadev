d68abc3e48e977af7258e7d23337ddbba1fe6e50
João Neto <jneto@MacJN2.lan>
2021-04-22 10:01:05 +0100

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,27 +3,44 @@
 #
 # Commands to be executed on cuda1 host
 #
-
-# ####################################################################################
-# cudadev shell
-# ####################################################################################
-
-sudo docker run -d --name cudadev-jneto --gpus all \
-    -v /home/jneto:/users/ml/home \
-    -v /dataf:/users/ml/home/data \
-    cudadev /bin/bash
-
-# ####################################################################################
-# cudadev jupyter
-# ####################################################################################
-
-# For jneto
-
-# For jranito
-
-# ####################################################################################
-# cudadev shell
-# ####################################################################################
+# 
+#
 
 # Access to cuda1 host (requires ssh setup for user)
 alias cuda1='ssh -i ~/.ssh/jn2020 -p 9022 -L 8001:localhost:8001 jneto@ml.dlogic.io'
+
+# ####################################################################################
+# cudadev shell
+# ####################################################################################
+
+sudo docker run -it --rm --name cudadev-jfn \
+    -v /home/jneto:/users/ml/nb \
+    -v /dataf:/users/ml/nb/data \
+    cudadev /bin/bash
+
+# --gpus all
+
+# ####################################################################################
+# cudadev jupyter
+#
+# Ports on host for cudadev-ju:
+#   jfn: 8201
+#   jvr: 8202
+#
+# ####################################################################################
+
+# For jfn
+docker rm -f cudadev-ju-jfn 2> /dev/null &&\
+docker run -dit --name cudadev-ju-jfn --restart unless-stopped \
+    -p 8201:8888 \
+    -v /home/jneto:/users/ml/nb \
+    -v /dataf:/users/ml/nb/data \
+    cudadev /bin/bash scripts/ju.sh
+
+# For jvr
+docker rm -f cudadev-ju-jvr 2> /dev/null &&\
+docker run -dit --name cudadev-ju-jfn --restart unless-stopped \
+    -p 8202:8888 \
+    -v /home/jneto:/users/ml/nb \
+    -v /dataf:/users/ml/nb/data \
+    cudadev /bin/bash scripts/ju.sh
